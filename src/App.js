a69912a2b58bc2a64735426ede1ac02cb40d323f
ozielsilveira <annaclaratxm@gmail.com>
2024-11-30 16:25:48 -0300

    fix: css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [formData, setFormData] = useState({
-    id: '',
-    age: '',
-    PackHistory: '',
-    MWT1Best: '',
-    FEV1: '',
-    FEV1PRED: '',
-    FVC: '',
-    FVCPRED: '',
-    CAT: '',
-    SGRQ: '',
-    AGEquartiles: '',
-    copd: '',
-    gender: '',
-    Diabetes: '',
-    hipertension: '',
-    AtrialFib: '',
-    IHD: ''
+    id: "",
+    age: "",
+    PackHistory: "",
+    MWT1Best: "",
+    FEV1: "",
+    FEV1PRED: "",
+    FVC: "",
+    FVCPRED: "",
+    CAT: "",
+    SGRQ: "",
+    AGEquartiles: "",
+    copd: "",
+    gender: "",
+    Diabetes: "",
+    hipertension: "",
+    AtrialFib: "",
+    IHD: "",
   });
 
   const [result, setResult] = useState(null);
@@ -28,47 +28,49 @@ function App() {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Convert formData values to numbers
-    const valuesArray = Object.values(formData).map(value => {
+    const valuesArray = Object.values(formData).map((value) => {
       const number = parseFloat(value);
       return isNaN(number) ? value : number;
     });
 
-    console.log('Sending data:', valuesArray);
+    console.log("Sending data:", valuesArray);
 
     try {
-      const response = await fetch('https://copd-model-production-5213.up.railway.app/predict', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ features: valuesArray }),
-      });
+      const response = await fetch(
+        "https://copd-model-production-5213.up.railway.app/predict",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ features: valuesArray }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
-      console.log('Response data:', data);
+      setResult(data);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <div className="App">
-      <h1>Prediction Form</h1>
-      <form onSubmit={handleSubmit}>
+      <h1>Formulário de Predição</h1>
+      <form onSubmit={handleSubmit} className="form-container">
         {Object.keys(formData).map((key) => (
-          <div key={key}>
+          <div key={key} className="form-group">
             <label htmlFor={key}>{key}:</label>
             <input
               type="text"
@@ -76,14 +78,17 @@ function App() {
               name={key}
               value={formData[key]}
               onChange={handleChange}
-            /><br /><br />
+              className="form-input"
+            />
           </div>
         ))}
-        <button type="submit">Submit</button>
+        <button type="submit" className="form-submit">
+          ENVIAR
+        </button>
       </form>
       {result !== null && (
-        <div>
-          <h2>Prediction Result:</h2>
+        <div className="result">
+          <h2>Resultado da Predição:</h2>
           <p>{result}</p>
         </div>
       )}
