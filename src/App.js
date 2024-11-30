e32fbb18b5dd18b4234d8194a715a13fd1922778
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-30 15:47:32 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    id: '',
+    age: '',
+    PackHistory: '',
+    MWT1Best: '',
+    FEV1: '',
+    FEV1PRED: '',
+    FVC: '',
+    FVCPRED: '',
+    CAT: '',
+    SGRQ: '',
+    AGEquartiles: '',
+    copd: '',
+    gender: '',
+    Diabetes: '',
+    hipertension: '',
+    AtrialFib: '',
+    IHD: ''
+  });
+
+  const [result, setResult] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Convert formData values to numbers
+    const valuesArray = Object.values(formData).map(value => {
+      const number = parseFloat(value);
+      return isNaN(number) ? value : number;
+    });
+
+    console.log('Sending data:', valuesArray);
+
+    try {
+      const response = await fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ features: valuesArray }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Response data:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Prediction Form</h1>
+      <form onSubmit={handleSubmit}>
+        {Object.keys(formData).map((key) => (
+          <div key={key}>
+            <label htmlFor={key}>{key}:</label>
+            <input
+              type="text"
+              id={key}
+              name={key}
+              value={formData[key]}
+              onChange={handleChange}
+            /><br /><br />
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+      {result !== null && (
+        <div>
+          <h2>Prediction Result:</h2>
+          <p>{result}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
